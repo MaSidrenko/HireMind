b3915bcfc77718dfadc71bc1165ed91dc8f979f5
MaSidrenko <nexon863@gmail.com>
2026-05-14 19:08:57 +0300

    feat[backend]: init asp.net project

---
+++ backend/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "HireMindAPI",
+        Version = "v1",
+        Description = "API для фриланс биржи HireMind"
+    });
+});
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
