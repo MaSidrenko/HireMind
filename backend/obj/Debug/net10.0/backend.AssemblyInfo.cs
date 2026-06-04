10068a08e71e0054c7d188854751f342e76ac2be
MaSidrenko <nexon863@gmail.com>
2026-06-04 20:36:43 +0300

    fix[git]: commit for merge

--- backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829814e67fba2a8c5307c7f5de9b4017e11188be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2523fa00911e0885701745adbf5879104748444f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
